259ce4d55ff657ee800ed56135e7b6bb8dc86a73
MrTorfis <oleksandr.pobizinsky@sytoss.com>
2023-03-11 20:05:51 +0300

    unit test was updated

--- src/test/java/com/sytoss/edu/elevator/cucumberTests/HelloFeatureTest.java
+++ src/test/java/com/sytoss/edu/elevator/cucumberTests/HelloFeatureTest.java
@@ -11,21 +11,21 @@ import org.springframework.http.ResponseEntity;
 
 
 public class HelloFeatureTest extends ControllerIntegrationTest {
-    ResponseEntity<String> resp;
+    ResponseEntity<String> response;
 
     @When("the user calls sayHello")
     public void theClientCallsSayHello () {
         String url = getBaseUrl() + "/api/helloWorld";
-        resp = new TestRestTemplate().exchange(url, HttpMethod.GET, null, String.class);
+        response = new TestRestTemplate().exchange(url, HttpMethod.GET, null, String.class);
     }
 
     @Then("operation is successful")
     public void operationSuccessful () {
-        Assertions.assertEquals(HttpStatus.valueOf(200), resp.getStatusCode());
+        Assertions.assertEquals(HttpStatus.valueOf(200), response.getStatusCode());
     }
 
     @Then("method sayHello should return {string}")
     public void returnedStringResp (String expect) {
-        Assertions.assertEquals(expect, resp.getBody());
+        Assertions.assertEquals(expect, response.getBody());
     }
 }
