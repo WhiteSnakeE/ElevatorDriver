514975d3bc543faaf9134398b83014ddfe27c609
MrTorfis <oleksandr.pobizinsky@sytoss.com>
2023-04-03 18:34:26 +0300

    Class Cabin was edited

--- src/main/java/com/sytoss/edu/elevator/bom/Cabin.java
+++ src/main/java/com/sytoss/edu/elevator/bom/Cabin.java
@@ -1,15 +1,17 @@
 package com.sytoss.edu.elevator.bom;
 
 import com.sytoss.edu.elevator.bom.enums.DoorState;
+import com.sytoss.edu.elevator.bom.enums.OverWeightState;
 
 public class Cabin extends Entity {
 
     private DoorState doorState=DoorState.CLOSED;
+    private OverWeightState overWeightState=OverWeightState.NOT_OVERWEIGHT;
     public void openDoor(){
         this.doorState=DoorState.OPENED;
     }
     public void closeDoor(){
         this.doorState=DoorState.CLOSED;
     }
-    public boolean isOverWeight(){return false;}
+    public boolean isOverWeight(){return overWeightState==OverWeightState.OVERWEIGHT;}
 }
