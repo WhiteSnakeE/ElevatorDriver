d4a36a7f041aefcc4d1ac5fcb9cd98e1c266944b
YehorBodenchuk <120697528+YehorBodenchuk@users.noreply.github.com>
2023-04-21 16:11:25 +0300

    [E23-22] Add swagger for end points.

--- src/main/java/com/sytoss/edu/elevator/controller/FloorController.java
+++ src/main/java/com/sytoss/edu/elevator/controller/FloorController.java
@@ -1,6 +1,8 @@
 package com.sytoss.edu.elevator.controller;
 
 import com.sytoss.edu.elevator.services.FloorService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,7 @@ public class FloorController {
 
     private final FloorService floorService;
 
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Success|OK"), @ApiResponse(responseCode = "400", description = "Bad request")})
     @PostMapping("/floorButton/{floorNumber}/up")
     public void goUpCabinRequest(
             @PathVariable(value = "floorNumber") int floorNumber) {
