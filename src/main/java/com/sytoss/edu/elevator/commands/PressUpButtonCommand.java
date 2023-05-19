027e9e61283d29ced75b548136807218457a3c4e
WhiteSnakeE <surtx0119@gmail.com>
2023-05-19 03:47:28 +0300

    [e23-25] fixed test and added floor check

--- src/main/java/com/sytoss/edu/elevator/commands/PressUpButtonCommand.java
+++ src/main/java/com/sytoss/edu/elevator/commands/PressUpButtonCommand.java
@@ -2,7 +2,6 @@ package com.sytoss.edu.elevator.commands;
 
 import com.sytoss.edu.elevator.bom.enums.Direction;
 import com.sytoss.edu.elevator.bom.house.House;
-import com.sytoss.edu.elevator.services.HouseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -18,8 +17,6 @@ public class PressUpButtonCommand implements Command {
 
     private final CommandManager commandManager;
 
-    private final HouseService houseService;
-
     @Override
     public void execute(HashMap<String, Object> params) {
         log.info("Start PressUpButton.execute COMMAND with params: {}", params);
@@ -27,9 +24,7 @@ public class PressUpButtonCommand implements Command {
         Direction direction = (Direction) params.get(DIRECTION_PARAM);
         House house = (House) params.get(HOUSE_PARAM);
         house.getElevatorDriver().addNewSequenceToOrder(numberFloor, direction);
-        houseService.updateOrderById(house.getId(), house.getElevatorDriver().getOrderSequenceOfStops());
         params.remove(FLOOR_NUMBER_PARAM);
-
         commandManager.getCommand(Command.FIND_NEAREST_CABIN_COMMAND).execute(params);
     }
 }
